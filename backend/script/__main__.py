1a6932927869e1f9e93ace7febb298c6583565b9
Yuri <yurevascopdf@gmail.com>
2026-06-11 16:39:36 -0300

    fix: adicionado o script de ETL para o titulo de tesouro direto

--- backend/script/__main__.py
+++ backend/script/__main__.py
@@ -1,5 +1,7 @@
 from src.treasury_direct import LTN, NTN
 from os import environ
+from datetime import date
+from pandas import date_range
 from requests import post, get, put
 from pandas import DataFrame,concat, to_datetime
 from dotenv import load_dotenv
@@ -36,11 +38,13 @@ else:
     df_all = DataFrame(response.json())
     year = df_all.data.astype("datetime64[ns]").dt.year.max()
 
+    year = [year] if year == date.today().year else date_range(start=str(year), end=date.today(), freq="YS").year.to_list()
     ltn = LTN()
-    df_ltn = ltn.extract_data(year)
+
+    df_ltn = ltn.extract_all_data(year)
     df_ltn["tipo"] = "LTN"
     ntn = NTN()
-    df_ntn = ntn.extract_data(year)
+    df_ntn = ntn.extract_all_data(year)
     df_ntn["tipo"] = "NTN"
     
     df = concat([df_ltn, df_ntn])
@@ -59,11 +63,14 @@ else:
 
     id_to_create = list(set(df.id.to_list()) - set(df_all.id.to_list()))
     id_to_update = list(set(df.id.to_list()) & set(df_all.id.to_list()))
+
     
     if id_to_create:
         
         response = post(environ["URL_CADASTRO_TITULOS"], json= df[df.id.isin(id_to_create)].to_dict("records"))
 
+        
     if id_to_update:
 
         response = put(environ["URL_ATUALIZAR_TITULOS"], json= df[df.id.isin(id_to_update)].to_dict("records"))
+    
